90fac9069899d470ddeb5625cdb1ac92cb8f92f4
GreerLG <45989880+GreerLG@users.noreply.github.com>
2024-02-21 22:38:37 +0000

    Added functional timer and restarting conditions

--- src/graph.hpp
+++ src/graph.hpp
@@ -13,24 +13,24 @@
 
 namespace std
 {
-  template <typename T, typename U>
-  struct hash<std::pair<T,U>>
-  {
-    std::size_t operator()(const std::pair<T,U>& n) const
-    {
-      return std::hash<T>{}(n.first) ^ std::hash<U>{}(n.second);
-    }
-  };
+	template <typename T, typename U>
+	struct hash<std::pair<T, U>>
+	{
+		std::size_t operator()(const std::pair<T, U>& n) const
+		{
+			return std::hash<T>{}(n.first) ^ std::hash<U>{}(n.second);
+		}
+	};
 }
 
-using node_t  = char;
-using edge_t  = std::pair<node_t, node_t>;
+using node_t = char;
+using edge_t = std::pair<node_t, node_t>;
 using coord_t = ai::Vector2;
 std::unordered_map<node_t, coord_t> node_info;
 std::unordered_map<edge_t, double>  edge_info;
 
 const float line_thickness = 5.0f;
-const float node_radius    = line_thickness * 2.0f;
+const float node_radius = line_thickness * 2.0f;
 
 inline double heuristic(node_t, node_t);
 #include "implementation.hpp" // in redblob-games
@@ -38,28 +38,28 @@ inline double heuristic(node_t, node_t);
 // neighbors and cost are needed by a_star_search
 struct Graph
 {
-  // std::unordered_map<node_t, std::pair<coord_t, std::vector<node_t>>> ?
-  // That would remove the need for node_info. Less readable?
-  std::unordered_map<node_t, std::vector<node_t>> nodes; // node + its n'bours
+	// std::unordered_map<node_t, std::pair<coord_t, std::vector<node_t>>> ?
+	// That would remove the need for node_info. Less readable?
+	std::unordered_map<node_t, std::vector<node_t>> nodes; // node + its n'bours
 
-  std::vector<node_t> neighbors(node_t id)
-  {
-    return nodes[id];
-  }
+	std::vector<node_t> neighbors(node_t id)
+	{
+		return nodes[id];
+	}
 
-  double cost(node_t current, node_t next)
-  {
-    return edge_info[{current,next}];
-  }
+	double cost(node_t current, node_t next)
+	{
+		return edge_info[{current, next}];
+	}
 };
 
 inline double heuristic(node_t next, node_t goal)
 {
-  const coord_t& npos = node_info[next];
-  const coord_t& gpos = node_info[goal];
-  const double x = std::abs(npos.x - gpos.x);
-  const double y = std::abs(npos.y - gpos.y);
-  return std::sqrt(x * x + y * y);
+	const coord_t& npos = node_info[next];
+	const coord_t& gpos = node_info[goal];
+	const double x = std::abs(npos.x - gpos.x);
+	const double y = std::abs(npos.y - gpos.y);
+	return std::sqrt(x * x + y * y);
 }
 
 #endif // _GRAPH_HPP_
--- src/pathfinder.cpp
+++ src/pathfinder.cpp
@@ -7,148 +7,185 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <cstdlib>
 
-std::vector<node_t> astar_pathfind(const Graph &g, node_t start, node_t goal)
+std::vector<node_t> astar_pathfind(const Graph& g, node_t start, node_t goal)
 {
-  std::unordered_map<node_t, node_t> came_from;
-  std::unordered_map<node_t, double> cost_so_far;
-  a_star_search(g, start, goal, came_from, cost_so_far);
-  std::vector<node_t> path = reconstruct_path(start, goal, came_from);
-  return path;
+	std::unordered_map<node_t, node_t> came_from;
+	std::unordered_map<node_t, double> cost_so_far;
+	a_star_search(g, start, goal, came_from, cost_so_far);
+	std::vector<node_t> path = reconstruct_path(start, goal, came_from);
+	return path;
 }
 
-unsigned int path_cost(const std::vector<node_t> &path)
+unsigned int path_cost(const std::vector<node_t>& path)
 {
-  double dcost = 0;
-  if (path.size() >= 2) // then we have some lines to draw
-  {
-    const int num_edges = path.size() - 1;
-    for (int i = 0; i < num_edges; i++)
-    {
-      dcost = dcost + edge_info[std::pair{path[i], path[i + 1]}];
-    }
-  }
+	double dcost = 0;
+	if (path.size() >= 2) // then we have some lines to draw
+	{
+		const int num_edges = path.size() - 1;
+		for (int i = 0; i < num_edges; i++)
+		{
+			dcost = dcost + edge_info[std::pair{ path[i], path[i + 1] }];
+		}
+	}
 
-  return static_cast<unsigned int>(dcost);
+	return static_cast<unsigned int>(dcost);
 }
 
-void HighlightPlayerPath(std::vector<std::pair<node_t, node_t>> &player_path_edges)
+void HighlightPlayerPath(std::vector<std::pair<node_t, node_t>>& player_path_edges)
 {
-  for (std::pair<node_t, node_t> edges : player_path_edges)
-  {
-    const coord_t &coord_n1 = node_info[edges.first];
-    const coord_t &coord_n2 = node_info[edges.second];
+	for (std::pair<node_t, node_t> edges : player_path_edges)
+	{
+		const coord_t& coord_n1 = node_info[edges.first];
+		const coord_t& coord_n2 = node_info[edges.second];
 
-    DrawLineEx(coord_n1, coord_n2, line_thickness + 10, GOLD);
-  }
+		DrawLineEx(coord_n1, coord_n2, line_thickness + 10, GOLD);
+	}
 };
 
 int main()
 {
-  const int w{1920}, h{1080}, half_w{w / 2}, half_h{h / 2}, gap{w / 8};
-  raylib::Window window{w, h, "Pathfinder"};
+	const int w{ 1920 }, h{ 1080 }, half_w{ w / 2 }, half_h{ h / 2 }, gap{ w / 8 };
+	raylib::Window window{ w, h, "Pathfinder" };
 
-  SetTargetFPS(60);
+	SetTargetFPS(60);
 
-  Graph g;
-  add_node(g, 'A', {half_w - gap, half_h});
-  add_node(g, 'B', {half_w, half_h});
-  add_node(g, 'C', {half_w, half_h - gap});
-  add_node(g, 'D', {half_w, half_h + gap});
-  add_node(g, 'E', {half_w + gap, half_h + gap});
-  add_node(g, 'F', {half_w + gap, half_h});
-  add_node(g, 'G', {half_w + (2 * gap), half_h - gap});
-  add_double_edge(g, 'A', 'B');
-  add_double_edge(g, 'B', 'C');
-  add_double_edge(g, 'B', 'D');
-  add_double_edge(g, 'C', 'A');
-  add_double_edge(g, 'D', 'E');
-  add_double_edge(g, 'D', 'A');
-  add_double_edge(g, 'E', 'B');
-  add_double_edge(g, 'B', 'F');
-  add_double_edge(g, 'C', 'F');
-  add_double_edge(g, 'C', 'G');
-  add_double_edge(g, 'F', 'G');
+	const char graphNodes[7] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 
-  int t{60}; // time
-  std::vector<node_t> player_path{};
+	Graph g;
+	add_node(g, 'A', { half_w - gap, half_h });
+	add_node(g, 'B', { half_w, half_h });
+	add_node(g, 'C', { half_w, half_h - gap });
+	add_node(g, 'D', { half_w, half_h + gap });
+	add_node(g, 'E', { half_w + gap, half_h + gap });
+	add_node(g, 'F', { half_w + gap, half_h });
+	add_node(g, 'G', { half_w + (2 * gap), half_h - gap });
+	add_double_edge(g, 'A', 'B');
+	add_double_edge(g, 'B', 'C');
+	add_double_edge(g, 'B', 'D');
+	add_double_edge(g, 'C', 'A');
+	add_double_edge(g, 'D', 'E');
+	add_double_edge(g, 'D', 'A');
+	add_double_edge(g, 'E', 'B');
+	add_double_edge(g, 'B', 'F');
+	add_double_edge(g, 'C', 'F');
+	add_double_edge(g, 'C', 'G');
+	add_double_edge(g, 'F', 'G');
 
-  // Variable player_path_edges, this is done so we can then highlight the player path
-  std::vector<std::pair<node_t, node_t>> player_path_edges{};
+	int t{ 60 }; // time
+	int currentTime = 2;
+	float timeAccum = 0.f;
+	std::vector<node_t> player_path{};
 
-  node_t start = 'A';
-  node_t end = 'G';
-  int tokens{2000}, score{}, high_score{}; // try with more/less tokens?
+	// Variable player_path_edges, this is done so we can then highlight the player path
+	std::vector<std::pair<node_t, node_t>> player_path_edges{};
 
-  while (!window.ShouldClose()) // Detect window close button or ESC key
-  {
-    BeginDrawing();
+	node_t start = 'A';
+	node_t end = 'G';
+	int tokens{ 2000 }, score{}, high_score{}; // try with more/less tokens?
+	bool noNodesLeft = false;
 
-    // Draw Text onto the screen
-    int text_x = 15;
-    int text_y = 15;
-    int font_size = 30;
+	while (!window.ShouldClose()) // Detect window close button or ESC key
+	{
+		timeAccum += GetFrameTime();
+		if (timeAccum >= 1.0f)
+		{
+			timeAccum = 0.f;
+			currentTime--;
+		}
+		
+		BeginDrawing();
 
-    DrawText(TextFormat("Score: %i", score), text_x, text_y, font_size, BLACK);
-    text_x += MeasureText(TextFormat("Score: %i", score), font_size) + font_size;
+		// Draw Text onto the screen
+		int text_x = 15;
+		int text_y = 15;
+		int font_size = 30;
 
-    DrawText(TextFormat("Tokens: %i", tokens), text_x, text_y, font_size, BLACK);
-    text_x += MeasureText(TextFormat("Tokens: %i", tokens), font_size) + font_size;
+		DrawText(TextFormat("Score: %i", score), text_x, text_y, font_size, BLACK);
+		text_x += MeasureText(TextFormat("Score: %i", score), font_size) + font_size;
 
-    DrawText(TextFormat("High Score: %i", high_score), text_x, text_y, font_size, GOLD);
+		DrawText(TextFormat("Tokens: %i", tokens), text_x, text_y, font_size, BLACK);
+		text_x += MeasureText(TextFormat("Tokens: %i", tokens), font_size) + font_size;
 
-    // Highlight Start Node
-    const coord_t &coord_startn = node_info[start];
-    DrawCircleV(coord_startn, node_radius + 5, GREEN);
+		DrawText(TextFormat("High Score: %i", high_score), text_x, text_y, font_size, GOLD);
+		text_x += MeasureText(TextFormat("High Score: %i", high_score), font_size) + font_size;
 
-    // Highlight End Node
-    const coord_t &coord_endn = node_info[end];
-    DrawCircleV(coord_endn, node_radius + 5, RED);
+		DrawText(TextFormat("Time: %i", currentTime), text_x, text_y, font_size, RED);
 
-    // Highlight Player Path
-    HighlightPlayerPath(player_path_edges);
+		// Highlight Start Node
+		const coord_t& coord_startn = node_info[start];
+		DrawCircleV(coord_startn, node_radius + 5, GREEN);
 
-    ClearBackground(LIGHTGRAY);
+		// Highlight End Node
+		const coord_t& coord_endn = node_info[end];
+		DrawCircleV(coord_endn, node_radius + 5, RED);
 
-    draw_graph(g);
+		// Highlight Player Path
+		HighlightPlayerPath(player_path_edges);
 
-    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-    {
-      if (auto opt = get_nearby_node(GetMousePosition()))
-      {
-        // Perform a check to see if first node is infact the start node
-        std::cout << node_info[*opt] << std::endl;
+		ClearBackground(LIGHTGRAY);
 
-        if (player_path.empty())
-        {
-          if (*opt == start)
-          {
-            player_path.push_back(*opt);
-          }
-        }
-        else
-        {
-          /*
-          Check the neighbours of the last added node, and ensure the new node is one of those,
-          this way preventing the player from building a path of unconnected nodes
-          */
-          std::vector<node_t> neighbours = g.neighbors(player_path.back());
-          int check = std::count(neighbours.begin(), neighbours.end(), *opt);
-          if (check > 0)
-          {
-            if (tokens >= edge_info[{player_path.back(), *opt}])
-            {
-              player_path_edges.push_back({player_path.back(), *opt});
-              tokens -= edge_info[{player_path.back(), *opt}];
-              player_path.push_back(*opt);
-            }
-          }
-        }
-      }
-    }
+		draw_graph(g);
 
-    EndDrawing();
-  }
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+		{
+			std::cout << "Left mouse button clicked" << std::endl;
+			auto coord = GetMousePosition();
+			auto opt = get_nearby_node(coord);
+			if (opt)
+			{
+				// Perform a check to see if first node is infact the start node
+				std::cout << node_info[*opt] << std::endl;
 
-  return 0;
+				if (player_path.empty())
+				{
+					if (*opt == start)
+					{
+						player_path.push_back(*opt);
+					}
+				}
+				else
+				{
+					/*
+					Check the neighbours of the last added node, and ensure the new node is one of those,
+					this way preventing the player from building a path of unconnected nodes
+					*/
+					std::vector<node_t> neighbours = g.neighbors(player_path.back());
+					int check = std::count(neighbours.begin(), neighbours.end(), *opt);
+					if (check > 0)
+					{
+						if (tokens >= edge_info[{player_path.back(), * opt}])
+						{
+							player_path_edges.push_back({ player_path.back(), *opt });
+							tokens -= edge_info[{player_path.back(), * opt}];
+							player_path.push_back(*opt);
+						}
+					}
+					else
+					{
+						noNodesLeft = true;
+					}
+				}
+			}
+		}
+
+
+		EndDrawing();
+
+		if (noNodesLeft || currentTime <= 0)
+		{
+			currentTime = t;
+			player_path.clear();
+			player_path_edges.clear();
+			start = graphNodes[rand() % 7];
+			end = graphNodes[rand() % 7];
+			while (start == end)
+			{
+				end = graphNodes[rand() % 7];
+			}
+		}
+	}
+
+	return 0;
 }
